1291693247c0f19318177d48076215b38fbba70c
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-23 09:34:51 +0000

    Code generation
    {
        "level": 1,
        "task_description": "add two radio button for selecting gender"
    }

--- components/search_input_1721727151.js
+++ components/search_input_1721727151.js
@@ -4,6 +4,16 @@ Vue.component('search_input_1721727151',
         <form class="space-y-4">
             <input type="text" placeholder="Field 1" class="w-full px-4 py-2 text-black placeholder-gray-500 bg-white border border-purple-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent">
             <input type="text" placeholder="Field 2" class="w-full px-4 py-2 text-black placeholder-gray-500 bg-white border border-purple-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent">
+            <div class="flex space-x-4">
+                <label class="flex items-center">
+                    <input type="radio" name="gender" value="male" class="mr-2 text-purple-600 focus:ring-purple-500">
+                    <span>Male</span>
+                </label>
+                <label class="flex items-center">
+                    <input type="radio" name="gender" value="female" class="mr-2 text-purple-600 focus:ring-purple-500">
+                    <span>Female</span>
+                </label>
+            </div>
             <button type="submit" class="w-full px-4 py-2 text-white bg-purple-600 rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 transition duration-300 ease-in-out">Submit</button>
         </form>
      </div>
